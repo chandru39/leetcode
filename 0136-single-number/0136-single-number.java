f91d6bd57a089f70931622ca3d4b991ae02999d0
chandru39 <23becse039@kahedu.edu.in>
2026-03-31 17:59:08 +0530

    Time: 13 ms (19.18%), Space: 46.7 MB (94.52%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-        for (int i = 0; i < nums.length; i++) {
-            result = result ^ nums[i];
-        }
-        return result;
+        HashMap<Integer,Integer>umap=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            umap.put(nums[i],umap.getOrDefault(nums[i],0)+1);
+        }for(int i=0;i<nums.length;i++){
+           if (umap.get(nums[i])==1){
+            return nums[i];
+           }
+        }return 0;
     }
 }
