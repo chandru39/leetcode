6d621f6b115baef8ee8fe29bdf15982e3a583958
chandru39 <23becse039@kahedu.edu.in>
2026-03-31 18:16:25 +0530

    Time: 4 ms (52.35%), Space: 47 MB (79.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{ i, j };
-                }
+        HashMap<Integer,Integer>umap=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int compliment=target-nums[i];
+            if(umap.containsKey(compliment)){
+                return new int[]{umap.get(compliment),i};
+            }else{
+                umap.put(nums[i],i);
             }
-        }
-        return new int[]{-1, -1};
+        }throw new IllegalArgumentException("No match");
     }
 }
