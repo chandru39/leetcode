aa9b50fed73ec6ddc7700c3f2280e975f9971934
chandru39 <23becse039@kahedu.edu.in>
2026-04-06 20:58:30 +0530

    Time: 1 ms (99.84%), Space: 55.9 MB (24.73%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int canditate=0;int count=0;
+        for(int num:nums){
+        if(count==0){
+            canditate=num;
+        }if(num==canditate){
+            count++;
+        }else{
+            count--;
+        }
+    }return canditate;
+}
+}
